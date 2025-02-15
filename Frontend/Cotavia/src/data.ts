b1aff856e179010bcb17cd894a29c317d6ede569
omar <omar25m2005@gmail.com>
2025-02-15 22:17:43 +0200

    BX

--- Frontend/Cotavia/src/data.ts
+++ Frontend/Cotavia/src/data.ts
@@ -1,7 +1,7 @@
 const devalopmentMode = false
 
 export default {
-    host: devalopmentMode? `http://127.0.0.1:8000/`: 'https://codavia-dhe8agaxheh4gmen.uaenorth-01.azurewebsites.net',
-    WsHost: devalopmentMode? `ws://127.0.0.1:8000/`: 'ws://codavia-dhe8agaxheh4gmen.uaenorth-01.azurewebsites.net',
+    host: devalopmentMode? `http://127.0.0.1:8000/`: 'https://codavia-dhe8agaxheh4gmen.uaenorth-01.azurewebsites.net/',
+    WsHost: devalopmentMode? `ws://127.0.0.1:8000/`: 'ws://codavia-dhe8agaxheh4gmen.uaenorth-01.azurewebsites.net/',
     devaloperMode: devalopmentMode
 }
